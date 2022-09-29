c001564cdccbd65c9e36a67ab961be27ce153b6b
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2022-09-29 21:31:39 +0200

    Add CI, license cleanup

--- test/run.vsim.sh
+++ test/run.vsim.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+# Paul Scheffler <paulsc@iis.ee.ethz.ch>
+
 set -e
 
 [ ! -z "$VSIM" ] || VSIM=vsim
